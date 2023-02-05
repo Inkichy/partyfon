ad9180a073cbb98d255f5aab185d897c2b23bc89
inkich <landsbergskiy@gmail.com>
2023-02-05 09:23:12 +0300

    initial commit

---
+++ utils/const.js
@@ -0,0 +1,9 @@
+export const ColorPalette = {
+    Main: '#049CE4',
+    MainHover: '#2c75ff',
+    Text: '#2d2d2d',
+    TextHover: '#2a2a2a',
+    Light: '#ffffff',
+    Dim: '#f1f1f1',
+    Darkness: '#000000',
+}
